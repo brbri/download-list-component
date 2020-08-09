8a1e940c0fb084b6607c7779f62905ef2e477b39
Brian H <43183351+brbri@users.noreply.github.com>
2020-08-09 11:15:16 -0700

    Update TableRowComponent.test.js

--- src/Components/TableRowComponent/TableRowComponent.test.js
+++ src/Components/TableRowComponent/TableRowComponent.test.js
@@ -13,7 +13,18 @@ describe('Check Row Events', () => {
     it('disabled checked box should be disabled', () => {
         const { container } = render(<TableRow row={{name: 'jon.exe', device: 'Jon', path: '\\Device\\HarddiskVolume2\\Windows\\System32\\jon.exe', status: 'scheduled'}} />);
         const checkBox = container.querySelector('input');
-        expect(checkBox).not.toBeChecked();
+        expect(checkBox.checked).toEqual(false);
         expect(checkBox.disabled).toEqual(true);
+    })
+
+    it('checked box should check', () => {
+        const { getByTestId, container } = render(<TableRow row={{name: 'jon.exe', device: 'Jon', path: '\\Device\\HarddiskVolume2\\Windows\\System32\\jon.exe', status: 'available'}} />);
+        const checkBox = container.querySelector('input');
+        expect(checkBox.checked).toEqual(false);
+        expect(getByTestId('active-row')).toHaveAttribute('id');
+        fireEvent.click(checkBox);
+        expect(checkBox.checked).toEqual(true);
+        fireEvent.click(checkBox);
+        expect(checkBox.checked).toEqual(false);
     })
 });
